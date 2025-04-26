a167cb86938447f9a18fffc3964e6889044acdf3
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2025-04-26 15:27:51 +1200

    initial API configuration and swagger setup

---
+++ aspnet/src/SDP.TaskManagement.WebHost/Program.cs
@@ -0,0 +1,68 @@
+using Microsoft.OpenApi.Models;
+
+using System.Reflection;
+
+namespace SDP.TaskManagement.WebHost;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+        // Swagger setup
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "TaskManagementWebApp API",
+                Description = "API for our SDP project.",
+                Contact = new OpenApiContact
+                {
+                    Name = "Nicolas Limbeger Ferreira",
+                    Url = new Uri("https://github.com/NicolasLFerreira")
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "MIT",
+                    Url = new Uri("https://opensource.org/licenses/MIT")
+                }
+            });
+
+            // XML comments
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            options.IncludeXmlComments(xmlPath);
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "TaskManagement API");
+                options.RoutePrefix = string.Empty;
+            });
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
