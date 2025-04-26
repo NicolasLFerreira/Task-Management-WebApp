d451d2f54d3df159d84c50d3f7ee8b3b8d7d3747
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2025-04-27 01:54:16 +1200

    Comment added

--- aspnet/src/SDP.TaskManagement.Application/Abstractions/IRepository.cs
+++ aspnet/src/SDP.TaskManagement.Application/Abstractions/IRepository.cs
@@ -1,5 +1,8 @@
 ﻿namespace SDP.TaskManagement.Application.Abstractions;
 
+/// <summary>
+/// Database access abstraction.
+/// </summary>
 public interface IRepository<TEntity, TId> where TEntity : class
 {
     Task AddAsync(TEntity entity);
