e47b66db33a87afe86eb39d5078653e2e9b68b2a
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2025-04-26 17:50:29 +1200

    Some cleaning

--- aspnet/src/SDP.TaskManagement.WebHost/Program.cs
+++ aspnet/src/SDP.TaskManagement.WebHost/Program.cs
@@ -11,9 +11,7 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
         builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
         // Swagger setup
         builder.Services.AddSwaggerGen(options =>
@@ -46,8 +44,6 @@ public class Program
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.MapOpenApi();
-
             app.UseSwagger();
             app.UseSwaggerUI(options =>
             {
