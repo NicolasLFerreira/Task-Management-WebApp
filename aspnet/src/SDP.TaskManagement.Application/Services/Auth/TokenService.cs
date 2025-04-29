df437af4a29c8b71cfb2187da4ecb552839ff3cb
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2025-04-30 02:33:59 +1200

    moved the tokenservice implementation to the Application assembly

--- aspnet/src/SDP.TaskManagement.Application/Services/Auth/TokenService.cs
+++ aspnet/src/SDP.TaskManagement.Application/Services/Auth/TokenService.cs
@@ -9,7 +9,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace SDP.TaskManagement.Infrastructure.Auth;
+namespace SDP.TaskManagement.Application.Services.Auth;
 
 public class TokenService : ITokenService
 {
