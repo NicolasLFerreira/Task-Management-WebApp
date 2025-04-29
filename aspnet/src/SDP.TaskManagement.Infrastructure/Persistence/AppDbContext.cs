4c6f557a149fb85bad889e2cc871c2a731f45a0a
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2025-04-29 23:21:36 +1200

    added ef specific commands in AppDbContext

--- aspnet/src/SDP.TaskManagement.Infrastructure/Persistence/AppDbContext.cs
+++ aspnet/src/SDP.TaskManagement.Infrastructure/Persistence/AppDbContext.cs
@@ -6,6 +6,8 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+    // create migration: dotnet ef migrations add {MigrationName} --startup-project ..\SDP.TaskManagement.WebHost\
+    // run migrations: dotnet ef database update
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
