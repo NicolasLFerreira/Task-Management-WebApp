8435e498c8a28c2aca1fa4d3c2c67875a0a1a9ab
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2025-04-29 22:09:13 +1200

    added sample repository for testing out the user creation. To be removed

---
+++ aspnet/src/SDP.TaskManagement.Application/Controllers/SampleRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using SDP.TaskManagement.Application.Abstractions;
+using SDP.TaskManagement.Domain.Entities;
+
+namespace SDP.TaskManagement.Application.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class SampleRepository : ControllerBase
+{
+    private readonly IRepository<User, Guid> _userRepository;
+
+    public SampleRepository(IRepository<User, Guid> userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    [HttpGet(Name = "GetUsers")]
+    public List<User> GetUsers()
+    {
+        return [.. _userRepository.GetQueryable()];
+    }
+}
