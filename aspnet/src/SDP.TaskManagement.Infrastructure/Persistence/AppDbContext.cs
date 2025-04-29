9a700d53a691d16bbca95afb687610ffffbd8200
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2025-04-29 20:22:41 +1200

    removed unnecessary dbsets

--- aspnet/src/SDP.TaskManagement.Infrastructure/Persistence/AppDbContext.cs
+++ aspnet/src/SDP.TaskManagement.Infrastructure/Persistence/AppDbContext.cs
@@ -1,35 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-
-using SDP.TaskManagement.Domain.Entities;
 
 namespace SDP.TaskManagement.Infrastructure.Persistence;
 
 public class AppDbContext : DbContext
 {
-    private readonly IConfiguration _configuration;
-
-    public AppDbContext(DbContextOptions<AppDbContext> options, IConfiguration configuration) : base(options)
-    {
-        _configuration = configuration;
-    }
-
-    // Tables
-    public DbSet<User> Tasks { get; set; }
-    public DbSet<TaskItem> TaskItems { get; set; }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
         base.OnModelCreating(modelBuilder);
     }
-
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //{
-    //    // Gets the connection string
-    //    optionsBuilder.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
-
-    //    // Applies migrations
-    //    Database.Migrate();
-    //}
 }
