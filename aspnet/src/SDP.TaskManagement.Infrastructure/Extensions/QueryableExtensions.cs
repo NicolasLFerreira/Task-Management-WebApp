233278bf1ab0b2f3f8c83dfa229fa5e9f8c688fa
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2025-05-08 14:59:15 +1200

    conditional where syntax

---
+++ aspnet/src/SDP.TaskManagement.Infrastructure/Extensions/QueryableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace SDP.TaskManagement.Infrastructure.Extensions;
+
+public static class QueryableExtensions
+{
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> queryable, bool condition, Expression<Func<T, bool>> predicate)
+    {
+        return condition
+            ? queryable.Where(predicate)
+            : queryable;
+    }
+}
