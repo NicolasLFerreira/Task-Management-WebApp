a6ee35d32929075588c5e120f5ca7280c2625e27
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2025-05-27 22:03:44 +1200

    fixed case

--- client/task-management-webapp/config/hey-api.ts
+++ client/task-management-webapp/config/hey-api.ts
@@ -2,6 +2,6 @@ import type { CreateClientConfig } from "../api-client/client.gen";
 
 export const createClientConfig: CreateClientConfig = (config) => ({
 	...config,
-	baseUrl: "https://localhost:7200/",
-	auth: () => "Bearer " + localStorage.getItem('auth_token')?.toString(),
+	baseURL: "https://localhost:7200/",
+	auth: () => "Bearer " + localStorage.getItem("auth_token")?.toString(),
 });
