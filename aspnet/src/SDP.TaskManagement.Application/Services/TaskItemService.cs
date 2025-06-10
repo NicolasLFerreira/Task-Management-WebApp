fe124756ba3bd5506587d084f4b7d3aaa476b5b8
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2025-06-10 21:01:06 +1200

    Updated specialised endpoint to utilise new service

---
+++ aspnet/src/SDP.TaskManagement.Application/Services/TaskItemService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using SDP.TaskManagement.Application.Abstractions;
+using SDP.TaskManagement.Application.Dtos;
+using SDP.TaskManagement.Application.Dtos.AssistanceDtos;
+using SDP.TaskManagement.Application.Extensions;
+using SDP.TaskManagement.Application.Mappers;
+using SDP.TaskManagement.Domain.Entities;
+
+namespace SDP.TaskManagement.Application.Services;
+
+public class TaskItemService : ITaskItemService
+{
+    private readonly IRepository<TaskItem> _repository;
+
+    public TaskItemService(IRepository<TaskItem> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<TaskItemDto>> FilterByProperty(long userId, FilterTaskItemInputDto input)
+    {
+        var result = await _repository.GetQueryable()
+            .Where(e => e.OwnerUserId == userId)
+            .WhereIf(input.Title != null, e => e.Title.Contains(input.Title!))
+            .WhereIf(input.Description != null, e => e.Description != null && e.Description.Contains(input.Description!))
+            .WhereIf(input.Status != null, e => e.ProgressStatus == input.Status)
+            .WhereIf(input.Priority != null, e => e.Priority == input.Priority)
+            .Select(e => e.ToDto())
+            .ToListAsync();
+
+        return result;
+    }
+
+    public async Task<List<TaskItemDto>> SearchByTitle(long userId, string titlePattern)
+    {
+        var result = await _repository.GetQueryable()
+            .Where(e => e.OwnerUserId == userId)
+            .Where(e => e.Title.Contains(titlePattern))
+            .Select(e => e.ToDto())
+            .ToListAsync();
+
+        return result;
+    }
+}
--- aspnet/src/SDP.TaskManagement.Web/Controllers/TaskItemSpecialisedController.cs
+++ aspnet/src/SDP.TaskManagement.Web/Controllers/TaskItemSpecialisedController.cs
@@ -4,8 +4,11 @@ using Microsoft.EntityFrameworkCore;
 
 using SDP.TaskManagement.Application.Abstractions;
 using SDP.TaskManagement.Application.Dtos;
+using SDP.TaskManagement.Application.Dtos.AssistanceDtos;
+using SDP.TaskManagement.Application.Extensions;
 using SDP.TaskManagement.Domain.Entities;
-using SDP.TaskManagement.Infrastructure.Extensions;
+
+using System.Security.Claims;
 
 namespace SDP.TaskManagement.Web.Controllers;
 
@@ -15,19 +18,24 @@ namespace SDP.TaskManagement.Web.Controllers;
 public class TaskItemSpecialisedController : ControllerBase
 {
     private readonly IRepository<TaskItem> _repository;
+    private readonly ITaskItemService _taskItemService;
 
-    public TaskItemSpecialisedController(IRepository<TaskItem> repository)
+    public TaskItemSpecialisedController(IRepository<TaskItem> repository, ITaskItemService taskItemService)
     {
         _repository = repository;
+        _taskItemService = taskItemService;
     }
 
     [HttpGet("{titlePattern}")]
     public async Task<ActionResult<List<TaskItemDto>>> SearchByTitle(string titlePattern)
     {
-        var result = await _repository
-            .GetQueryable()
-            .Where(e => e.Title.Contains(titlePattern))
-            .ToListAsync();
+        //var result = await _repository.GetQueryable()
+        //    .Where(e => e.Title.Contains(titlePattern))
+        //    .ToListAsync();
+
+        var userId = GetCurrentUserId();
+
+        var result = await _taskItemService.SearchByTitle(userId, titlePattern);
 
         return Ok(result);
     }
@@ -35,22 +43,28 @@ public class TaskItemSpecialisedController : ControllerBase
     [HttpPost]
     public async Task<ActionResult<List<TaskItemDto>>> FilterByProperty([FromBody] FilterTaskItemInputDto input)
     {
-            var result = await _repository
-            .GetQueryable()
-            .WhereIf(input.Title != null, e => e.Title.Contains(input.Title!))
-            .WhereIf(input.Description != null, e => e.Description != null && e.Description.Contains(input.Description!))
-            .WhereIf(input.Status != null, e => e.ProgressStatus == input.Status)
-            .WhereIf(input.Priority != null, e => e.Priority == input.Priority)
-            .ToListAsync();
+        //var result = await _repository.GetQueryable()
+        //    .Where(e => e.OwnerUserId == 1)
+        //    .WhereIf(input.Title != null, e => e.Title.Contains(input.Title!))
+        //    .WhereIf(input.Description != null, e => e.Description != null && e.Description.Contains(input.Description!))
+        //    .WhereIf(input.Status != null, e => e.ProgressStatus == input.Status)
+        //    .WhereIf(input.Priority != null, e => e.Priority == input.Priority)
+        //    .ToListAsync();
+
+        var userId = GetCurrentUserId();
+
+        var result = await _taskItemService.FilterByProperty(userId, input);
 
         return Ok(result);
     }
-}
 
-public sealed class FilterTaskItemInputDto()
-{
-    public string? Title { get; set; }
-    public string? Description { get; set; }
-    public TaskItemStatus? Status { get; set; }
-    public TaskItemPriority? Priority { get; set; }
+    private long GetCurrentUserId()
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+        if (userIdClaim == null)
+            throw new UnauthorizedAccessException("User ID not found in token");
+
+        return long.Parse(userIdClaim.Value);
+    }
 }
